1955c13c6a26f9524467a1f81439b740ba2e8811
Neg <negramones@gmail.com>
2025-02-13 16:44:14 -0300

    Adicionar a Visualização de usuarios no Django Admin
    Signed-off-by: Neg <negramones@gmail.com>

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nome_empresa', 'email_corporativo', 'solucao_criptografia', 'telefone')
+    search_fields = ('user__username', 'email_corporativo', 'nome_empresa')
+
+admin.site.register(UserProfile, UserProfileAdmin)
