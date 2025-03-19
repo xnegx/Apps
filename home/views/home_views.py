339bc9f924a1ea76eb4534467c319a17252b4138
Neg <negramones@gmail.com>
2025-03-19 13:04:23 -0300

    remove função de cadastro
    Signed-off-by: Neg <negramones@gmail.com>

--- home/views/home_views.py
+++ home/views/home_views.py
@@ -38,21 +38,3 @@ def home(request):
 def apps_view(request):
     return render(request, 'home/apps.html')
 
-def signup_view(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()  # Salva o usuário padrão
-            # Salva os dados adicionais no UserProfile
-            UserProfile.objects.create(
-                user=user,
-                nome_empresa=form.cleaned_data['nome_empresa'],
-                email_corporativo=form.cleaned_data['email_corporativo'],
-                solucao_criptografia=form.cleaned_data['solucao_criptografia'],
-                telefone=form.cleaned_data['telefone']
-            )
-            messages.success(request, 'Cadastro realizado com sucesso!')
-            return redirect('home')  # Redireciona para a página inicial
-    else:
-        form = SignUpForm()
-    return render(request, 'home/signup.html', {'form': form})
