13b059744138704ce03edbbf9f73d92d1737a600
Neg <negramones@gmail.com>
2025-03-21 15:15:41 -0300

    adiciona checkbox de validaçao de politica antes de entrar
    Signed-off-by: Neg <negramones@gmail.com>

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # Rota para a página inicial
     path('apps/', views.apps_view, name='apps'),  # Página Apps (após login)
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),  # Redireciona para a página inicial após o logout
-    path('signup/', views.signup_view, name='signup'),  # Rota para o cadastro
     path('csr_decoder/', views.csr_decoder, name='csr_decoder'), # Rota para o csr decoder
     path('header/', views.header, name='header'),# Rota para o header
     path("certificados/", views.certificate_views.listar_certificados, name="certificados"), # Rota para lista de certificados
